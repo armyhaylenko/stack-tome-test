8333fe0815129c9eba6c149d5c18c222bcd00fdc
armyhaylenko <armyhaylenko@gmail.com>
2020-06-09 19:33:49 +0300

    List all RSS links in Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,13 +9,28 @@ import scala.io.Source._
 import java.io.File
 
 object Main extends zio.App {
+  
+  //sbt did not want to include the file with links to the assembly task, had to put the links here
+  val links = List("https://112.ua/rss/analytics/index.rss",
+  "https://112.ua/rss/statji/index.rss",
+  "https://112.ua/rss/mnenie/index.rss",
+  "https://112.ua/rss/interview/index.rss",
+  "https://112.ua/rss/politika/index.rss",
+  "https://112.ua/rss/ato/index.rss",
+  "https://112.ua/rss/ekonomika/index.rss",
+  "https://112.ua/rss/novosti-kanala/channel.rss?type=index",
+  "https://112.ua/rss/sport/index.rss",
+  "https://112.ua/rss/kiev/index.rss",
+  "https://112.ua/rss/mir/index.rss",
+  "https://112.ua/rss/avarii-chp/index.rss",
+  "https://112.ua/rss/kriminal/index.rss")
 
   def print[A](zio: Task[A]) = zio.flatMap(x => putStrLn(x.toString()))
 
   override def run(args: List[String]): ZIO[zio.ZEnv,Nothing, ExitCode] = {
     AsyncHttpClientZioBackend().flatMap { implicit backend =>
-      val allNews = News(fromFile(new File("src\\main\\resources\\RSSList.txt")).getLines().toList).data
-      val trends = Trends("https://trends.google.com/trends/trendingsearches/daily/rss?geo=UA", strictMode = true).data
+      val allNews = News(links).data
+      val trends = Trends("https://trends.google.com/trends/trendingsearches/daily/rss?geo=UA", strictMode = false).data
       print(NewsTrendsMatcher(allNews, trends)) *> backend.close()
     } repeat (Schedule.spaced(300 seconds)) exitCode
   }
